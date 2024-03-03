c0bd21aea3f130181465ec871d0e94370e25960e
anasahammad <anasahammadsarker@gmail.com>
2024-03-03 17:07:22 +0600

    fetched all post apiand dynamic the mark read

---
+++ script.js
@@ -0,0 +1,98 @@
+const allPostContainer = document.getElementById('all-posts-container');
+const readCounter = document.getElementById('read-count');
+const readCounteContainer = document.getElementById('read-count-container');
+
+let readCount = 0;
+
+const allPosts = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await res.json();
+    const posts = data.posts;
+    posts.forEach((post) =>{
+        // console.log(post)
+
+        const div = document.createElement('div');
+        div.className = 'bg-[#797dfc1a] rounded-3xl p-6 my-6';
+        div.innerHTML = `
+        <div class="flex flex-col gap-4 lg:flex-row">
+                            <div class="flex flex-col lg:flex-row gap-6">
+                               
+                                <div class=" relative">
+                                    <img src="${post.image}"  class="rounded-2xl w-[72px] h-[72px]"/>
+
+                                    <div class="absolute w-3 h-3 bg-green-500 rounded-full left-[62px]  lg:left-[55px]  top-0"></div>
+                                  </div>
+                                  
+
+
+
+                                  <div>
+
+                                    <div class="flex  gap-6">
+                                        <p># ${post.category}</p>
+                                        <p>Author : ${post.author.name}</p>
+                                    </div>
+                                    <h5 class="text-[20px] font-mulish font-bold">${post.title}</h5>
+                                    <p class="py-6 text-[#12132d99]">${post.description}</p>
+
+
+                                    <hr class="my-5 border-dotted bg-[#12132d40] ">
+
+
+                                    <div class="flex justify-between">
+                                        <div class="flex gap-3 lg:gap-5">
+                                            <div class="flex justify-center items-center gap-1 lg:gap-3">
+                                                <img src="images/tabler-icon-message-2.svg" alt="">
+                                                <p>${post.comment_count}</p>
+                                            </div>
+                                            <div class="flex justify-center items-center gap-1 lg:gap-3">
+                                                <img src="images/tabler-icon-eye.svg" alt="">
+                                                <p>${post.view_count}</p>
+                                            </div>
+                                            <div class="flex justify-center items-center  lg:gap-3">
+                                                <img src="images/tabler-icon-clock-hour-9.svg" alt="">
+                                                <p>${post.posted_time} min</p>
+                                            </div>
+                                        </div>
+            
+                                        <button onclick="markRead('${post.title}', '${post.view_count}')" id="mark-read">
+                                            <img src="images/email 1.svg" alt="">
+                                        </button>
+                                    </div>
+
+                                  </div>    
+                            </div>
+                        </div>
+        
+        `
+
+        
+        allPostContainer.appendChild(div)
+        
+    })
+
+    
+   
+
+    
+}
+const markRead = (title, view)=>{
+    readCount = readCount + 1;
+    readCounter.innerText = readCount;
+    const div = document.createElement('div');
+    div.className = 'bg-white flex p-4 rounded-2xl justify-between my-4';
+    div.innerHTML = `
+    <div>
+                            <h5 class="text-[#12132D] font-medium font-mulish">${title}</h5>
+                        </div>
+                        <div class="flex justify-center items-center gap-1">
+                            <img src="images/tabler-icon-eye.svg" alt="">
+                            <p>${view}</p>
+                        </div>
+    
+`
+readCounteContainer.appendChild(div) 
+}
+
+
+allPosts()
